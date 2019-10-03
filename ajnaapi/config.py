16fc7dc69269b6c66f2b6dacfb75708c7774e7ec
brasilico.ivan@gmail.com <Iv@n1234>
2019-10-03 09:24:41 -0300

    flake8

--- ajnaapi/config.py
+++ ajnaapi/config.py
@@ -4,14 +4,16 @@
 
 # sys.path.insert(0, COMMONS_PATH)
 
-from ajna_commons.flask.conf import MONGODB_URI, SECRET
 from pymongo import MongoClient
+
+from ajna_commons.flask.conf import MONGODB_URI, SECRET
 
 
 class Production:
     TESTING = False
     SECRET = SECRET
     db = MongoClient(host=MONGODB_URI).test
+
 
 class Testing:
     TESTING = True
