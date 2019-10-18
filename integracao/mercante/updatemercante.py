ab63513d87c066c62f2a120fe92d9f73e81c31a5
IvanBrasilico <brasilico.ivan@gmail.com>
2019-10-18 15:42:36 -0300

    Lote como parâmetro

--- integracao/mercante/updatemercante.py
+++ integracao/mercante/updatemercante.py
@@ -1,3 +1,4 @@
+import sys
 from sqlalchemy import create_engine
 
 from ajna_commons.flask.conf import SQL_URI
@@ -6,8 +7,12 @@ from integracao.mercante.resume_mercante import mercante_resumo
 
 
 def do():
+    lote = 100
+    if len(sys.argv) > 1:
+        lote = int(sys.argv[1])
+        print('Lote de %s arquivos' % lote)
     sql = create_engine(SQL_URI)
-    xml_para_mercante(sql)
+    xml_para_mercante(sql, lote)
     mercante_resumo(sql)
 
 
